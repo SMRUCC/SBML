6daea51bb99ad40be136b62ecb34d28bc4df3bfb
xieguigang <xie.guigang@gmail.com>
2016-04-19 04:38:24 +0800

    level2 and level3 files simple parser

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LANS.SystemsBiology.Assembly.SBML.ExportServices;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = LANS.SystemsBiology.Assembly.SBML.Level2.XmlFile.Load(@"F:\1.13.RegPrecise_network\FBA\xcam314565\19.0\data\metabolic-reactions.xml");
+            var dd = file.Model.listOfReactions.First();
+            double l = dd.LowerBound;
+            double u = dd.UpperBound;
+            var rxns = LANS.SystemsBiology.Assembly.SBML.ExportServices.KEGG.GetReactions(file,true);
+            Console.Read();
+        }
+    }
+}
