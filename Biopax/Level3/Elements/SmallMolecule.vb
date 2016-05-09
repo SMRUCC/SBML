3acafeac331eef4b838f75fc90d26f5681cfe31d
xieguigang <xie.guigang@gmail.com>
2016-05-10 02:09:02 +0800

    data types

---
+++ Biopax/Level3/Elements/SmallMolecule.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.DocumentFormat.RDF
+
+<XmlType("SmallMolecule")>
+Public Class SmallMolecule : Inherits RDFEntity
+
+    Public Property displayName As String
+
+End Class
+
+Public Class displayName : Inherits EntityProperty
+
+End Class
+Public Class name : Inherits EntityProperty
+
+End Class
+Public Class cellularLocation : Inherits EntityProperty
+
+End Class
+Public Class entityReference : Inherits EntityProperty
+
+End Class
+Public Class comment : Inherits EntityProperty
+End Class
+Public Class xref : Inherits EntityProperty
+
+End Class
+Public Class dataSource : Inherits EntityProperty
+
+End Class
+Public Class term : Inherits EntityProperty
+
+End Class
