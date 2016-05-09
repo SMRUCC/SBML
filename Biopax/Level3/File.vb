2287cbb97aec4511be11b956cee866e522ed3c6d
xieguigang <xie.guigang@gmail.com>
2016-05-10 02:30:21 +0800

    doc node process

--- Biopax/Level3/File.vb
+++ Biopax/Level3/File.vb
@@ -1,13 +1,15 @@
 ﻿Imports System.Text
 Imports System.Xml.Serialization
 Imports Biopax.MetaCyc.Biopax.Level3.Elements
+Imports Microsoft.VisualBasic.DocumentFormat.RDF.DocumentElements
 
 Namespace MetaCyc.Biopax.Level3
 
     ''' <summary>
     ''' Biopax RDF xml file.
+    ''' &lt;rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:bp="http://www.biopax.org/release/biopax-level3.owl#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:xsd="http://www.w3.org/2001/XMLSchema#" xml:base="http://www.reactome.org/biopax/56/159879#">
     ''' </summary>
-    Public Class File
+    Public Class File : Inherits RDF
 
         Public Property Owl As owlOntology
         <XmlElement("SmallMolecule")> Public Property SmallMolecules As SmallMolecule()
