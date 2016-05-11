fbdec7ae490ae9deb4cc974510b0e1142d5606ba
xieguigang <xie.guigang@gmail.com>
2016-05-11 17:26:09 +0800

    add biopax elements

--- Biopax/Level3/Schema/FunctionalProperty.vb
+++ Biopax/Level3/Schema/FunctionalProperty.vb
@@ -18,11 +18,38 @@ Namespace Level3.Schema
         Public Property [Class] As [Class]
     End Class
 
-    Public Class [Class] : Inherits RDFProperty
+    Public Class [Class] : Inherits RDFEntity
         Public Property unionOf As unionOf
+        ''' <summary>
+        ''' rdfs:subClassOf
+        ''' </summary>
+        ''' <returns></returns>
+        <XmlElement> Public Property subClassOf As subClassOf()
+        ''' <summary>
+        ''' owl:disjointWith
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property disjointWith As RDFProperty
+    End Class
+
+    Public Class subClassOf : Inherits RDFEntity
+        Public Property Restriction As Restriction
+    End Class
+
+    Public Class Restriction : Inherits RDFEntity
+        Public Property onProperty As RDFProperty
+        Public Property allValuesFrom As RDFProperty
+
     End Class
 
     Public Class unionOf : Inherits RDFProperty
         <XmlElement> Public Property [Class] As RDFProperty()
+    End Class
+
+    Public Class InverseFunctionalProperty : Inherits RDFEntity
+        <XmlElement> Public Property type As RDFProperty()
+        Public Property domain As domain
+        Public Property inverseOf As RDFProperty
+
     End Class
 End Namespace
