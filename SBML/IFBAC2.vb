d3170cfca2ce7ffdc56f10ecf13f5dac433ccc6d
xieguigang <xie.guigang@gmail.com>
2016-06-29 05:22:49 +0800

    apply license

--- SBML/IFBAC2.vb
+++ SBML/IFBAC2.vb
@@ -1,4 +1,31 @@
-﻿Imports LANS.SystemsBiology.ComponentModel.EquaionModel
+﻿#Region "Microsoft.VisualBasic::bb5ad6442289c7fb079e7459ad8e5f11, ..\SBML\IFBAC2.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports LANS.SystemsBiology.ComponentModel.EquaionModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 
 Namespace FLuxBalanceModel
